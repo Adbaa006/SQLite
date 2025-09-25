1979a3d5073208d8b055441d43d9c08e2586870c
Adbaa006 <adbaa007@hotmail.com>
2025-09-25 08:36:27 +0200

    Meny-funksjon

--- butikk.py
+++ butikk.py
@@ -20,45 +20,48 @@ c.execute("""
           )
 """)
 
-print(input("Hei, hva vil du gjøre? \nBestille bøker \nSelge bøker \nSe lager \nSe salg \nSkriv her: "))
+def legge_til_vare():
+    bok = input("Skriv inn boken som skal bestilles: ")
+    pris = input("Skriv inn hva boken koster: ")
+    antall_varer = input("Skriv hvor mange bøker som skal bestilles: ")
+    c.execute("INSERT INTO inventar (navn, pris, antall) VALUES(?,?,?)", (bok, pris, antall_varer))
+    databasekobling.commit()
+
+def slett_vare():
+    vare_id = input("Skriv id til varen som skal slettes: ")
+    c.execute("DELETE FROM inventar WHERE id = ?", (vare_id))
+    databasekobling.commit()
+
+inn = ""
+
+while inn != "q":
+    print("""
+    MENY
+    1. Legg til vare
+    2. Slett vare
+          """)
+    inn = input(": ")
+    match inn:
+        case "1":
+            legge_til_vare()
+        case "2":
+            slett_vare()
+    
 
 
-bestille = "Bestille bøker"
-selge = "Selge bøker"
-lager = "Se lager"
-salg = "Se salg"
+# Selge bøker
+vare_id = input("Skriv inn ID-en til boken som skal selges: ")
+dato = input("Skriv inn dato salget blir gjort: ")
+antall_salg = input("Skriv inn antall som blir solgt: ")
 
+c.execute("INSERT INTO salg (vare_id, dato, antall) VALUES(?,?,?)", (vare_id, dato, antall_salg))
+# Vise varelager
+c.execute("SELECT * FROM inventar")
+# Vise salg
+c.execute("SELECT * FROM salg")
 
-while True:
-    if input == bestille:
-        # Bestille bøker
-        bok = input("Skriv inn boken som skal bestilles: ")
-        pris = input("Skriv inn hva boken koster: ")
-        antall_varer = input("Skriv hvor mange bøker som skal bestilles: ")
-
-        c.execute("INSERT INTO inventar (navn, pris, antall) VALUES(?,?,?)", (bok, pris, antall_varer))
-        break
-    elif input == selge:
-        # Selge bøker
-        vare_id = input("Skriv inn ID-en til boken som skal selges: ")
-        dato = input("Skriv inn dato salget blir gjort: ")
-        antall_salg = input("Skriv inn antall som blir solgt: ")
-
-        c.execute("INSERT INTO salg (vare_id, dato, antall) VALUES(?,?,?)", (vare_id, dato, antall_salg))
-        break
-    elif input == lager:
-        # Vise varelager
-        c.execute("SELECT * FROM inventar")
-        break
-    elif input == salg:
-        # Vise salg
-        c.execute("SELECT * FROM salg")
-        break
-    else:
-        print("Ugyldig input, sjekk skrivefeil og prøv igjen")
-        break
 
 
 print(c.fetchall())
-databasekobling.commit()
+
 databasekobling.close()
